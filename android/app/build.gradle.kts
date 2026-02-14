51b90865d94c3484e9b57aacc40a3790fa621247
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2026-02-14 12:34:38 +0545

    Upgrade package name/bundle id

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,29 +1,21 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.sharmadhiraj.flutter_foundation.flutter_foundation"
+    namespace = "com.sharmadhiraj.flutter_foundation"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.sharmadhiraj.flutter_foundation.flutter_foundation"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.sharmadhiraj.flutter_foundation"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +24,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
--- android/app/src/main/kotlin/com/sharmadhiraj/flutter_foundation/flutter_foundation/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.sharmadhiraj.flutter_foundation.flutter_foundation
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
